49786577cfd31f03e8e5d66a4a38837031367151
Cassius <111204494+cass-us@users.noreply.github.com>
2026-09-24 21:21:32 +0200

    Uncomment Navbar import in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-//import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Services from "./components/Services";
 import About from "./components/About";
